d7def7d3edfe73da1303e7faa56db2a901f71ed8
Joe Devine <joseph.devine.mt@gmail.com>
2023-10-19 16:54:31 -0600

    configured constraints for image file in article form

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -8,19 +8,38 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ArticleFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('title')
-            ->add('imageFile', FileType::class, [
-                'mapped' => false,
-                'required' => false
+        /** @var Article|null $article */
+        $article = $options['data'] ?? null;
+        $isEdit = $article && $article->getId();
+
+        $builder->add('title');
+
+        $imageConstraints = [
+            new Image([
+                'maxSize' => '5M'
             ])
-            ->add('save', SubmitType::class)
-        ;
+        ];
+
+        if(!$isEdit || !$article->getImageFilename()){
+            $imageConstraints[] = new NotNull([
+                'message' => 'Please upload an image.'
+            ]);
+        }
+
+        $builder->add('imageFile', FileType::class, [
+            'mapped' => false,
+            'required' => false,
+            'constraints' => $imageConstraints
+        ]);
+
+        $builder->add('save', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
